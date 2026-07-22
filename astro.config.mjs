73c67cefef5bede0cc34e41835d7a6b2cf221f92
Nicholas Nazario <Nazario9847@gmail.com>
2026-07-21 23:04:56 -0400

    fix: fix routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://nicholas-nazario.github.io",
-  base: "/",
+  base: "/Nicholas-Nazario",
 });
--- src/data/photoCollections.ts
+++ src/data/photoCollections.ts
@@ -13,11 +13,11 @@ export const photoCollections: PhotoCollection[] = [
     title: "Birds",
     description: "The birds I’ve seen in New York City, and beyond.",
     photos: [
-      "/photos/birds/American%20Woodcock.JPG",
-      "/photos/birds/Black%20Crowned%20Night%20Heron.JPG",
-      "/photos/birds/Brown%20Thrasher.JPG",
-      "/photos/birds/Red%20Tailed%20Hawk.JPG",
-      "/photos/birds/Red%20Winged%20Blackbird.JPG",
+      "photos/birds/American%20Woodcock.JPG",
+      "photos/birds/Black%20Crowned%20Night%20Heron.JPG",
+      "photos/birds/Brown%20Thrasher.JPG",
+      "photos/birds/Red%20Tailed%20Hawk.JPG",
+      "photos/birds/Red%20Winged%20Blackbird.JPG",
     ],
   },
   {
@@ -26,9 +26,9 @@ export const photoCollections: PhotoCollection[] = [
     title: "Argus C4",
     description: "Random pictures taken on an old Argus C4",
     photos: [
-      "/photos/argus-c4/Brooklyn%20Bridge.JPG",
-      "/photos/argus-c4/Central%20Park%20in%20Snow.JPG",
-      "/photos/argus-c4/The%20Beresford.JPG",
+      "photos/argus-c4/Brooklyn%20Bridge.JPG",
+      "photos/argus-c4/Central%20Park%20in%20Snow.JPG",
+      "photos/argus-c4/The%20Beresford.JPG",
     ],
   },
 ];
