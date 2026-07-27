d43b029e11d72fe8895d91a9b5e362ff026c5acd
Nicholas Nazario <Nazario9847@gmail.com>
2026-07-27 19:25:41 -0400

    feat: adds titles & subtitles to photos

--- src/data/photoCollections.ts
+++ src/data/photoCollections.ts
@@ -3,7 +3,13 @@ export interface PhotoCollection {
   number: string;
   title: string;
   description: string;
-  photos: string[];
+  photos: Photo[];
+}
+
+export interface Photo {
+  src: string;
+  title: string;
+  subtitle: string;
 }
 
 export const photoCollections: PhotoCollection[] = [
@@ -13,11 +19,11 @@ export const photoCollections: PhotoCollection[] = [
     title: "Birds",
     description: "The birds I’ve seen in New York City, and beyond.",
     photos: [
-      "photos/birds/American%20Woodcock.JPG",
-      "photos/birds/Black%20Crowned%20Night%20Heron.JPG",
-      "photos/birds/Brown%20Thrasher.JPG",
-      "photos/birds/Red%20Tailed%20Hawk.JPG",
-      "photos/birds/Red%20Winged%20Blackbird.JPG",
+      { src: "photos/birds/American%20Woodcock.JPG", title: "American Woodcock", subtitle: "Spotted in Bryant Park early in the spring" },
+      { src: "photos/birds/Black%20Crowned%20Night%20Heron.JPG", title: "Black-crowned Night Heron", subtitle: "Spotted at the Harlem Meer" },
+      { src: "photos/birds/Brown%20Thrasher.JPG", title: "Brown Thrasher", subtitle: "Spotted on the Magothy River" },
+      { src: "photos/birds/Red%20Tailed%20Hawk.JPG", title: "Red-tailed Hawk", subtitle: "Spotted from my apartment window! The hawk was just perched on my deck" },
+      { src: "photos/birds/Red%20Winged%20Blackbird.JPG", title: "Red-winged Blackbird", subtitle: "Spotted at the Harlem Meer" },
     ],
   },
   {
@@ -26,9 +32,9 @@ export const photoCollections: PhotoCollection[] = [
     title: "Argus C4",
     description: "Random pictures taken on an old Argus C4",
     photos: [
-      "photos/argus-c4/Brooklyn%20Bridge.JPG",
-      "photos/argus-c4/Central%20Park%20in%20Snow.JPG",
-      "photos/argus-c4/The%20Beresford.JPG",
+      { src: "photos/argus-c4/Brooklyn%20Bridge.JPG", title: "Brooklyn Bridge", subtitle: "Walking towards Brooklyn; Shot with an Argus C4" },
+      { src: "photos/argus-c4/Central%20Park%20in%20Snow.JPG", title: "Central Park in Snow", subtitle: "Taken on a snowy evening in Central Park; Shot with an Argus C4" },
+      { src: "photos/argus-c4/The%20Beresford.JPG", title: "The Beresford", subtitle: "The Beresford apartment complex on the UWS; Shot with an Argus C4" },
     ],
   },
 ];
